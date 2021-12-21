29aaef300d3aff9995476e0774416107e2f52c08
SaeedKaboli <websitesaeed@gmail.com>
2021-12-22 00:33:56 +0330

    Finish

---
+++ src/App.js
@@ -0,0 +1,446 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      
+
+        <section className="banner-area" id="home">
+          <div className="container">
+            <div className="row fullscreen d-flex align-items-center justify-content-start">
+              <div className="banner-content col-lg-12">
+                <h1 className="text-right">
+                  روز خود را با یک قهوه <br />
+                  خوشمزه شروع کنید
+                </h1>
+                <a href="#" className="primary-btn text-uppercase buy-btn">
+                  سفارش دهید
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="video-sec-area pb-100 pt-40" id="about">
+          <div className="container">
+            <div className="row justify-content-start align-items-center text-right">
+              <div className="col-lg-6 video-right justify-content-center align-items-center d-flex">
+                <div className="overlay overlay-bg"></div>
+                <a className="play-btn" href="#">
+                  <img className="img-fluid" src="img/play-icon.png" alt="" />
+                </a>
+              </div>
+              <div className="col-lg-6 video-left">
+                <h6>لورم ایپسوم طرحی ساختگی</h6>
+                <h1>
+                  لورم ایپسوم طرحی ساختگی <br />
+                  متن طراحی لورم ایپسوم
+                </h1>
+                <p>
+                  <span>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است.
+                  </span>
+                </p>
+                <p>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                  استفاده از طراحان گرافیک است.لورم ایپسوم متن ساختگی با تولید
+                  سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                </p>
+                <img className="img-fluid" src="img/signature.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="menu-area section-gap" id="coffee">
+          <div className="container">
+            <div className="row d-flex justify-content-center">
+              <div className="menu-content pb-60 col-lg-10">
+                <div className="title text-center">
+                  <h1 className="mb-10">
+                    چه قهوه ای میل دارین براتون سرو کنیم
+                  </h1>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="single-menu">
+                  <div className="title-div justify-content-between d-flex">
+                    <h4>کاپوچینو</h4>
+                    <p className="price float-right">49 تومان</p>
+                  </div>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="gallery-area section-gap" id="gallery">
+          <div className="container">
+            <div className="row d-flex justify-content-center">
+              <div className="menu-content pb-60 col-lg-10">
+                <div className="title text-center">
+                  <h1 className="mb-10">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است.
+                  </h1>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-4">
+                <a href="img/g1.jpg" className="img-pop-home">
+                  <img className="img-fluid" src="img/g1.jpg" alt="" />
+                </a>
+                <a href="img/g2.jpg" className="img-pop-home">
+                  <img className="img-fluid" src="img/g2.jpg" alt="" />
+                </a>
+              </div>
+              <div className="col-lg-8">
+                <a href="img/g3.jpg" className="img-pop-home">
+                  <img className="img-fluid" src="img/g3.jpg" alt="" />
+                </a>
+                <div className="row">
+                  <div className="col-lg-6">
+                    <a href="img/g4.jpg" className="img-pop-home">
+                      <img className="img-fluid" src="img/g4.jpg" alt="" />
+                    </a>
+                  </div>
+                  <div className="col-lg-6">
+                    <a href="img/g5.jpg" className="img-pop-home">
+                      <img className="img-fluid" src="img/g5.jpg" alt="" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="review-area section-gap" id="review">
+          <div className="container">
+            <div className="row d-flex justify-content-center">
+              <div className="menu-content pb-60 col-lg-10">
+                <div className="title text-center">
+                  <h1 className="mb-10">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت
+                  </h1>
+                  <p>لورم ایپسوم متن ساختگی با تولید سادگی</p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-6 col-md-6 single-review">
+                <img src="img/r1.png" alt="" />
+                <div className="title d-flex flex-row">
+                  <h4>لورم ایپسوم</h4>
+                  <div className="star">
+                    <i className="fa fa-star checked"></i>
+                    <i className="fa fa-star checked"></i>
+                    <i className="fa fa-star checked"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                  </div>
+                </div>
+                <p>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                  استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
+                  مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
+                  تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای
+                  کاربردی می باشد
+                </p>
+              </div>
+              <div className="col-lg-6 col-md-6 single-review">
+                <img src="img/r2.png" alt="" />
+                <div className="title d-flex flex-row">
+                  <h4>لورم ایپسوم</h4>
+                  <div className="star">
+                    <i className="fa fa-star checked"></i>
+                    <i className="fa fa-star checked"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                  </div>
+                </div>
+                <p>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                  استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
+                  مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
+                  تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای
+                  کاربردی می باشد
+                </p>
+              </div>
+            </div>
+            <div className="row counter-row">
+              <div className="col-lg-3 col-md-6 single-counter">
+                <h1 className="counter">2536</h1>
+                <p>مشتری ها راضی</p>
+              </div>
+              <div className="col-lg-3 col-md-6 single-counter">
+                <h1 className="counter">7562</h1>
+                <p>کل محصولات</p>
+              </div>
+              <div className="col-lg-3 col-md-6 single-counter">
+                <h1 className="counter">2013</h1>
+                <p>فنجون قهوه</p>
+              </div>
+              <div className="col-lg-3 col-md-6 single-counter">
+                <h1 className="counter">10536</h1>
+                <p>تعداد کل</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="blog-area section-gap" id="blog">
+          <div className="container">
+            <div className="row d-flex justify-content-center">
+              <div className="menu-content pb-60 col-lg-10">
+                <div className="title text-center">
+                  <h1 className="mb-10">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                  </h1>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-6 col-md-6 single-blog">
+                <img className="img-fluid" src="img/b1.jpg" alt="" />
+                <ul className="post-tags">
+                  <li>
+                    <a href="#">سفر</a>
+                  </li>
+                  <li>
+                    <a href="#">سبک زندگی</a>
+                  </li>
+                </ul>
+                <a href="#">
+                  <h4>لورم ایپسوم طراحان ساختگی نامفهوم</h4>
+                </a>
+                <p>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                  استفاده از طراحان گرافیک است
+                </p>
+                <p className="post-date">10 فروردین 1398</p>
+              </div>
+              <div className="col-lg-6 col-md-6 single-blog">
+                <img className="img-fluid" src="img/b2.jpg" alt="" />
+                <ul className="post-tags">
+                  <li>
+                    <a href="#">سفر</a>
+                  </li>
+                  <li>
+                    <a href="#">سبک زندگی</a>
+                  </li>
+                </ul>
+                <a href="#">
+                  <h4>لورم ایپسوم طراحان ساختگی نامفهوم</h4>
+                </a>
+                <p>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                  استفاده از طراحان گرافیک است
+                </p>
+                <p className="post-date">10 فروردین 1398</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <footer className="footer-area section-gap">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-5 col-md-6 col-sm-6 footer">
+                <div className="single-footer-widget">
+                  <h6>درباره ما</h6>
+                  <p>
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک استلورم ایپسوم متن ساختگی با
+                    تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                    گرافیک است
+                  </p>
+                  <p className="footer-text text-center">
+                    کپی رایت &copy;
+                    <script>document.write(new Date().getFullYear());</script>
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-5  col-md-6 col-sm-6">
+                <div className="single-footer-widget">
+                  <h6>خبرنامه</h6>
+                  <p>از آخرین خبرهای ما مطلع باشید</p>
+                  <div className="" id="mc_embed_signup">
+                    <form
+                      target="_blank"
+                      novalidate="true"
+                      action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                      method="get"
+                      className="form-inline"
+                    >
+                      <input
+                        className="form-control"
+                        name="EMAIL"
+                        placeholder="ایمیل"
+                        onfocus="this.placeholder = ''"
+                        onblur="this.placeholder = 'ایمیل خود را وارد کنید '"
+                        required=""
+                        type="email"
+                      />
+                      <button className="click-btn btn btn-default">
+                        <i
+                          className="fa fa-long-arrow-right"
+                          aria-hidden="true"
+                        ></i>
+                      </button>
+                      <div style={{ position: "absolute", left: "-5000px" }}>
+                        <input
+                          name="b_36c4fd991d266f23781ded980_aefe40901a"
+                          tabindex="-1"
+                          value=""
+                          type="text"
+                        />
+                      </div>
+
+                      <div className="info pt-20"></div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-6 col-sm-6 social-widget">
+                <div className="single-footer-widget">
+                  <h6>ما را دنیال کنید</h6>
+
+                  <div className="footer-social d-flex align-items-center">
+                    <a href="#">
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                    <a href="#">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                    <a href="#">
+                      <i className="fa fa-dribbble"></i>
+                    </a>
+                    <a href="#">
+                      <i className="fa fa-behance"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </React.Fragment>
+    );
+  }
+}
